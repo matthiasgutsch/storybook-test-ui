458d6a96fb8129dbaf13d0562dfd65dfbee394f0
Matthias Gutsch <MGutsch@datacolor.com>
2026-01-21 10:29:37 +0100

    ok

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,20 @@
 import { StorybookConfig } from '@analogjs/storybook-angular';
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-docs",
-    "@storybook/addon-vitest",
-    "@storybook/addon-a11y"
+    '@storybook/addon-links',
+    '@storybook/addon-docs',
+    '@storybook/addon-vitest',
+    '@storybook/addon-a11y',
+    '@chromatic-com/storybook'
   ],
   framework: {
-    name: "@analogjs/storybook-angular",
+    name: '@analogjs/storybook-angular',
     options: {},
   },
   docs: {},
-  staticDirs: ["../public"]
+  staticDirs: ['../public'],
 };
 
 export default config;
